95fc9ebec21fd1890aeb1235b7f1e3dab181919f
Tiger <billath131908@gmail.com>
2025-03-23 15:35:27 +0200

    1.12.3 Get User by Username

--- UserManagement/Controllers/UserController.cs
+++ UserManagement/Controllers/UserController.cs
@@ -47,5 +47,23 @@ namespace UserManagement.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, new ApiResponse { Status = "Error", Message = ex.Message });
             }
         }
+
+        [HttpGet("GetUserByUsername/{username}")]
+        public ActionResult<RegisterUserDto> GetUserByUsername(string username)
+        {
+            try
+            {
+                var user = _userService.FindUserByUsername(username);
+                if (user == null)
+                {
+                    return NotFound(new ApiResponse { Status = "Error", Message = "User not found" });
+                }
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ApiResponse { Status = "Error", Message = ex.Message });
+            }
+        }
     }
 }
