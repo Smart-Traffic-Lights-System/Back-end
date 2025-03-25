a9d793d381392707287b2d0fa09fc9ae40f7cf3c
Tiger <billath131908@gmail.com>
2025-03-25 20:15:40 +0200

    1.33.4.1 Update docker containers and run at Linux

--- UserManagementService/UserStore/obj/Debug/net9.0/UserStore.AssemblyInfo.cs
+++ UserManagementService/UserStore/obj/Debug/net9.0/UserStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d79b88703929f49266f11c7a563488519fb13182")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7effa4b726a1ceda1b61628ab572787a04e6392c")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
